9e0bd4d2ca78e9b5e04a58030709e55045a70a3e
Kotaro Hara <koe.bluebear@gmail.com>
2011-09-25 00:51:28 -0400

    finished write-ups

--- p1/q2.py
+++ p1/q2.py
@@ -22,8 +22,25 @@ def main():
 	runClassifier.trainTestSet(dt.DT({'maxDepth': 5}), datasets.CFTookCG)
 	print 
 	
-	curve = runClassifier.learningCurveSet(dt.DT({'maxDepth': 5}), datasets.CFTookAI)
-	runClassifier.plotCurve('DT on AI', curve)
+	#curve = runClassifier.learningCurveSet(dt.DT({'maxDepth': 5}), datasets.CFTookAI)
+	#runClassifier.plotCurve('DT on AI', curve)
+	
+	#curve = runClassifier.hyperparamCurveSet(dt.DT({'maxDepth': 5}), 'maxDepth', [1,2,3,4,5,6,7,8,9,10], datasets.CFTookAI)
+	#runClassifier.plotCurve( 'DT on AI (hyperparameter)', curve )
+	
+	print "WU4:"
+	h = dt.DT({'maxDepth': 3})
+	h.train( datasets.CFTookCG.X, datasets.CFTookCG.Y )
+	print h
+
+	print "0", datasets.CFDataRatings.courseNames[6], datasets.CFDataRatings.courseIds[6]
+	print "1-left", datasets.CFDataRatings.courseNames[34], datasets.CFDataRatings.courseIds[34]
+	print "1-left-2-left", datasets.CFDataRatings.courseNames[48], datasets.CFDataRatings.courseIds[48]
+	print "1-left-2-right", datasets.CFDataRatings.courseNames[27], datasets.CFDataRatings.courseIds[27]
+	print "1-right", datasets.CFDataRatings.courseNames[54], datasets.CFDataRatings.courseIds[54]
+	print "1-right-2-left", datasets.CFDataRatings.courseNames[32], datasets.CFDataRatings.courseIds[32]
+	print "1-right-2-left", datasets.CFDataRatings.courseNames[53], datasets.CFDataRatings.courseIds[53]
+
 	return
 	
 if __name__=="__main__":
